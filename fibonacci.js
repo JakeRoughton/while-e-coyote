957a5e80d4d1bef2a3f6695a569741ee9ae7d13f
JakeRoughton <112968037+JakeRoughton@users.noreply.github.com>
2022-09-22 10:37:10 -0400

    Add files via upload

--- fibonacci.js
+++ fibonacci.js
@@ -1,21 +1,20 @@
 const prompt = require('prompt-sync')({sigint: true});
 
-    let num = Number(prompt("Enter your starting number: "))
-    let num1 = 0
-    let num2 = 1
-    let burrito = Number()
-    let i =0
-
-    while(i < num){
-        if (i===0){
-            console.log(0);
-        }    else if ( i === 1){
-                console.log(1);
-            } else{
-                burrito = num1 + num2;
-                console.log(num1 + num2);
-                num1 = num2;
-                num2 = burrito;
-            }
-            i++;
-        }
+let n = Number(prompt("Enter a number"));
+let num1 = 0;
+let num2 = 1;
+let i = 0;
+let newNum;
+while (i < n) {
+    if (i === 0) {
+        console.log(i);
+    } else if(i === 1){
+        console.log(i);
+    } else{
+        newNum = num1 + num2
+        console.log("newNum is ", newNum);
+        num1 = num2
+        num2 = newNum
+    }
+    i++
+}
